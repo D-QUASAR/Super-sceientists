89cce940e22493c93d3281b09c9071d92222fef9
D-QUASAR <63438816+D-QUASAR@users.noreply.github.com>
2020-05-02 11:34:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-//setup date
-const date = (document.getElementById(
-    "date"
-).innerHTML = new Date().getFullYear());
-// setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
-// show nav
-navBtn.addEventListener("click", () => {
-    navbar.classList.add("showNav");
-});
-// close nav
-navClose.addEventListener("click", () => {
-    navbar.classList.remove("showNav");
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList);
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+  links.classList.toggle("show-links");
 });
